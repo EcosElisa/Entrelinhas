4911de21646ac49913c56742158fba4c3236c47f
RafaDSan <rafaeldss12@gmail.com>
2025-09-11 17:50:30 -0300

    Add api comunication

---
+++ pages/api/v1/search_books/index.js
@@ -0,0 +1,7 @@
+import searchBooks from "../../../../models/search_book";
+
+export default async function getHandler(request, response) {
+  const { q } = request.query;
+  const books = await searchBooks(q);
+  return response.status(200).json(books);
+}
